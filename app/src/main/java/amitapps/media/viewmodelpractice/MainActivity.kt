c8385285ca75d124c31d3af5f1d9596d05f9d74b
amitYadavDev <amit8955461017@gmail.com>
2024-01-12 12:21:54 +0530

    increment value using viewModel

--- app/src/main/java/amitapps/media/viewmodelpractice/MainActivity.kt
+++ app/src/main/java/amitapps/media/viewmodelpractice/MainActivity.kt
@@ -3,18 +3,26 @@ package amitapps.media.viewmodelpractice
 import amitapps.media.viewmodelpractice.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
     var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.btn.setOnClickListener{
-            count++
-            binding.text.text = count.toString()
+            viewModel.incrementValue()
+        }
+
+        binding.btn1.setOnClickListener {
+            viewModel.decrementValue()
+        }
+        viewModel.counter.observe(this) {
+            binding.text.text = it.toString()
         }
     }
 }
--- app/src/main/java/amitapps/media/viewmodelpractice/MainViewModel.kt
+++ app/src/main/java/amitapps/media/viewmodelpractice/MainViewModel.kt
@@ -1,9 +1,24 @@
 package amitapps.media.viewmodelpractice
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    var counter = 0
+    var count = 0
+
+    private val _counter = MutableLiveData<Int>()
+    var counter: LiveData<Int> = _counter
+
+    fun incrementValue() {
+        count++
+        _counter.postValue(count)
+    }
+
+    fun decrementValue() {
+        count--
+        _counter.postValue(count)
+    }
 
 
 
